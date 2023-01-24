f81cd66ae55f42e3997f2b7ae12a3bb6fb34f01b
Andre <andre.christian.silva@gmail.com>
2023-01-23 21:08:25 -0800

    Added Profile routes.

--- app/api/profile_routes.py
+++ app/api/profile_routes.py
@@ -17,3 +17,40 @@ def get_a_profile(id):
     profile = Profile.query.get(id)
 
     return profile.to_dict()
+
+# Create a Profile
+@profile_routes.route('/', methods=['POST'])
+def create_profile():
+    profile_data = request.json
+
+    new_profile = Profile(**profile_data, user_id = current_user.id)
+
+    db.session.add(new_profile)
+    db.session.commit()
+
+    return new_profile.to_dict()
+
+# Edit a Profile
+@profile_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_profile(id):
+    form = ProfileForm()
+    profile = Profile.query.get(id)
+
+    profile.username = form.data['username']
+    profile.about_me = form.data['about_me']
+
+    db.session.commit()
+
+    return profile.to_dict()
+
+# Delete a Profile
+@profile_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_profile(id):
+    profile = Profile.query.get(id)
+
+    db.session.delete(profile)
+    db.session.commit()
+
+    return {"message": "deleted successfully"}
